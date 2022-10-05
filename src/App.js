e8a2332cb3f21377dbd76c5033c9fa4e84f8a9ca
cyd losekann <cydnel@gmail.com>
2022-10-05 14:41:17 -0300

    update

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Cards from './Cards';
+// import Avatar from './Avatar';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Cards />
+      {/* <Avatar /> */}
+      <Footer />
+    </>
+  );
+};
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="footer d-flex">
+    {/* <div class="logo">
+      <img src={require('../images/logo.png')} id="icon" alt="logo" />
+    </div> */}
+    <div className="footer-copyright">
+      <p style={{color: "#112232"}}>Cyd Losekann © 2022</p>
+    </div>
+  </div>
+  )
+}
+
+export default Footer;
